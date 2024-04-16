2981d0e56f8140bd6314401f6a05453216685534
z.shokri <z.shokri@mohaymen.ir>
2024-04-16 14:22:23 +0330

    Created Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function HomePage() {
-  return (
-      <div>
-        <h1> Welcome to Your Note My Friend :) </h1>
-      </div>
-  );
+    return (
+        <div className='flex flex-col text-center font-mono font-extrabold text-gray-600'>
+            <h1 className='p-10 text-2xl'> Welcome to Your Note My Friend :) </h1>
+            <div className='group'>
+                <Link
+                    className='p-2 w-auto underline transition text-xl rounded-xl hover:bg-fuchsia-400 hover:text-gray-50'
+                    href='./notes-list'> Go To Your List </Link>
+                <span hidden className='pl-3 text-xl group-hover:inline'>😉</span>
+            </div>
+        </div>
+    );
 }
